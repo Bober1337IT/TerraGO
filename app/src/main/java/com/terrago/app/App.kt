7b51d92bddae872ae65cd1be0fe13ef0c5b727bd
Bober1337IT <bartekpietras03@gmail.com>
2025-11-21 15:50:39 +0100

    Clean MainActivity and add App

---
+++ app/src/main/java/com/terrago/app/App.kt
@@ -0,0 +1,12 @@
+package com.terrago.app
+
+import androidx.compose.runtime.Composable
+import com.terrago.app.db.TerraGoDatabase
+import com.terrago.app.ui.theme.TerraGOTheme
+
+@Composable
+fun App(database: TerraGoDatabase) {
+    TerraGOTheme {
+
+    }
+}
