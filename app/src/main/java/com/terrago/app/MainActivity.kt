34bf125ea3fefb4ddc704cd6d0fcb53ae6afe93b
Bober1337IT <bartekpietras03@gmail.com>
2026-03-13 15:51:02 +0100

    Refactor database initialization to use singleton

--- app/src/main/java/com/terrago/app/MainActivity.kt
+++ app/src/main/java/com/terrago/app/MainActivity.kt
@@ -5,15 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.view.WindowCompat
-import com.terrago.app.database.DatabaseDriverFactory
-import com.terrago.app.db.TerraGoDatabase
+import com.terrago.app.database.DatabaseProvider
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            val database = TerraGoDatabase(DatabaseDriverFactory(LocalContext.current).createDriver())
+            val database = DatabaseProvider.getDatabase(LocalContext.current)
             App(database)
         }
     }
